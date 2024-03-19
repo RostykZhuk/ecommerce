756cc567ebde5c2508514300f097c2e64e3b4e14
RostykZhuk <rostukzhuk@gmail.com>
2024-03-19 12:05:23 +0200

    completed sidebar

--- trello-clone/app/(platform)/(dashboard)/organization/layout.tsx
+++ trello-clone/app/(platform)/(dashboard)/organization/layout.tsx
@@ -1,10 +1,11 @@
 import { ReactNode } from "react";
+import { Sidebar } from "../_components/sidebar";
 
 const OrganizationLayout = ({ children }: { children: ReactNode }) => {
   return (
     <main className="pt-20 md:pt-24 px-4 max-w-6xl 2xl:max-w-screen-xl">
       <div className="flex gap-x-7">
-        <div className="w-64 shrink-0 hidden md:block">{/* <Side /> */}</div>
+        <div className="w-64 shrink-0 hidden md:block"><Sidebar/></div>
         {children}
       </div>
     </main>
